03c466b1e93b965dde7b97823af162b1079c3779
Samara Alves <samaraalves778@gmail.com>
2025-09-20 23:02:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,50 +6,50 @@ const INSTAGRAM_USUARIO = 'jottavburguer';
 // Ponto central para gerenciamento de itens. Altere este objeto para modificar o cardápio.
 const cardapioData = {
     hamburgueres: [
-        { id: 1, nome: 'Smash Original', preco: 18.99, descricao: 'Pão, carne 80g prensada na chapa, queijo muçarela, cebola caramelizada, alface, tomate e molho da casa.', imagem: '[URL-DA-IMAGEM-SMASH-ORIGINAL.jpg]', disponivel: true },
-        { id: 2, nome: 'Smash Duplo', preco: 24.99, descricao: 'Pão, 2 carnes 80g prensadas na chapa, 2 queijos muçarela, cebola caramelizada, alface, tomate e molho da casa.', imagem: '[URL-DA-IMAGEM-SMASH-DUPLO.jpg]', disponivel: true },
-        { id: 3, nome: 'JottaV Básico', preco: 14.99, descricao: 'Pão, carne 80g prensada na chapa com cebola, queijo muçarela e molho da casa.', imagem: '[URL-DA-IMAGEM-JOTTAV-BASICO.jpg]', disponivel: true },
-        { id: 4, nome: 'JottaV Classic', preco: 25.99, descricao: 'Pão, carne 150g, molho barbecue, tomate, alface, bacon e queijo muçarela.', imagem: '[URL-DA-IMAGEM-JOTTAV-CLASSIC.jpg]', disponivel: true },
-        { id: 5, nome: 'Búrguer do Xerife', preco: 22.99, descricao: 'Pão, carne de 120g, (queijo muçarela ou requeijão longa) e farofa (bacon ou alho).', imagem: '[URL-DA-IMAGEM-BURGUE-DO-XERIFE.jpg]', disponivel: true },
-        { id: 6, nome: 'Duplo Brutão', preco: 37.99, descricao: 'Pão, 2 carnes de 120g, (queijo muçarela ou requeijão longa), molho da casa, 2 fatias de bacon.', imagem: '[URL-DA-IMAGEM-DUPLO-BRUTAO.jpg]', disponivel: true },
-        { id: 7, nome: 'Calabresa Prime', preco: 26.99, descricao: 'Pão, molho, alface, tomate, calabresa, queijo, cebola caramelizada, (carne ou frango).', imagem: '[URL-DA-IMAGEM-CALABRESA-PRIME.jpg]', disponivel: true },
-        { id: 8, nome: 'Burguer Salame', preco: 28.99, descricao: 'Pão, requeijão, carne e salame.', imagem: '[URL-DA-DA-IMAGEM-BURGUE-SALAME.jpg]', disponivel: true },
-        { id: 9, nome: 'Calabresa Básico', preco: 15.00, descricao: 'Pão, molho da casa, calabresa e queijo muçarela.', imagem: '[URL-DA-IMAGEM-CALABRESA-BASICO.jpg]', disponivel: true }
+        { id: 1, nome: 'Smash Original', preco: 18.99, descricao: 'Pão, carne 80g prensada na chapa, queijo muçarela, cebola caramelizada, alface, tomate e molho da casa.', imagem: 'imagens_hamburgueres/smash_original.jpg', disponivel: true },
+        { id: 2, nome: 'Smash Duplo', preco: 24.99, descricao: 'Pão, 2 carnes 80g prensadas na chapa, 2 queijos muçarela, cebola caramelizada, alface, tomate e molho da casa.', imagem: 'imagens_hamburgueres/smash_duplo.jpg', disponivel: true },
+        { id: 3, nome: 'JottaV Básico', preco: 14.99, descricao: 'Pão, carne 80g prensada na chapa com cebola, queijo muçarela e molho da casa.', imagem: 'imagens_hamburgueres/jottav_basico.jpg', disponivel: true },
+        { id: 4, nome: 'JottaV Classic', preco: 25.99, descricao: 'Pão, carne 150g, molho barbecue, tomate, alface, bacon e queijo muçarela.', imagem: 'imagens_hamburgueres/jottav_classic.jpg', disponivel: true },
+        { id: 5, nome: 'Búrguer do Xerife', preco: 22.99, descricao: 'Pão, carne de 120g, (queijo muçarela ou requeijão longa) e farofa (bacon ou alho).', imagem: 'imagens_hamburgueres/burguer_xerife.jpg', disponivel: true },
+        { id: 6, nome: 'Duplo Brutão', preco: 37.99, descricao: 'Pão, 2 carnes de 120g, (queijo muçarela ou requeijão longa), molho da casa, 2 fatias de bacon.', imagem: 'imagens_hamburgueres/duplo_brutao.jpg', disponivel: true },
+        { id: 7, nome: 'Calabresa Prime', preco: 26.99, descricao: 'Pão, molho, alface, tomate, calabresa, queijo, cebola caramelizada, (carne ou frango).', imagem: 'imagens_hamburgueres/calabresa_prime.jpg', disponivel: true },
+        { id: 8, nome: 'Burguer Salame', preco: 28.99, descricao: 'Pão, requeijão, carne e salame.', imagem: 'imagens_hamburgueres/burguer_salame.jpg', disponivel: true },
+        { id: 9, nome: 'Calabresa Básico', preco: 15.00, descricao: 'Pão, molho da casa, calabresa e queijo muçarela.', imagem: 'imagens_hamburgueres/calabresa_basico.jpg', disponivel: true }
     ],
     acompanhamentos: [
-        { id: 10, nome: 'Batata Palito', preco: 11.99, descricao: 'Batata com batata.', imagem: '[URL-DA-IMAGEM-BATATA-PALITO.jpg]', disponivel: true },
-        { id: 11, nome: 'Batata com Bacon', preco: 17.99, descricao: 'Batata com bacon.', imagem: '[URL-DA-IMAGEM-BATATA-BACON.jpg]', disponivel: true }
+        { id: 10, nome: 'Batata Palito', preco: 11.99, descricao: 'Batata com batata.', imagem: 'imagens_acompanhamentos/batata_palito.jpg', disponivel: true },
+        { id: 11, nome: 'Batata com Bacon', preco: 17.99, descricao: 'Batata com bacon.', imagem: 'imagens_acompanhamentos/batata_bacon.jpg', disponivel: true }
     ],
     combos: [
-        { id: 12, nome: 'Combo Econômico', preco: 31.99, descricao: '1 Smash Original + batata frita + refrigerante lata.', imagem: '[URL-DA-IMAGEM-COMBO-ECONOMICO.jpg]', disponivel: true },
-        { id: 13, nome: 'Combo do Chef', preco: 41.99, descricao: '1 JottaV Classic + batata frita + refrigerante lata.', imagem: '[URL-DA-IMAGEM-COMBO-DO-CHEF.jpg]', disponivel: true },
-        { id: 14, nome: 'Combo Smash Duplo', preco: 37.99, descricao: '1 Smash Duplo + batata frita + refrigerante lata.', imagem: '[URL-DA-IMAGEM-COMBO-SMASH-DUPLO.jpg]', disponivel: true },
-        { id: 15, nome: 'Combo Premium', preco: 53.99, descricao: '1 Duplo Brutão + batata frita + refrigerante lata.', imagem: '[URL-DA-IMAGEM-COMBO-PREMIUM.jpg]', disponivel: true },
-        { id: 16, nome: 'Casal R$ 49,99', preco: 49.99, descricao: '2 Smash Original + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-CASAL-49.jpg]', disponivel: true },
-        { id: 17, nome: 'Casal R$ 63,99', preco: 63.99, descricao: '2 JottaV Classic + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-CASAL-63.jpg]', disponivel: true },
-        { id: 18, nome: 'Casal R$ 61,99', preco: 61.99, descricao: '2 Smash Duplo + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-CASAL-61.jpg]', disponivel: true },
-        { id: 19, nome: 'Casal R$ 87,99', preco: 87.99, descricao: '2 Duplo Brutão + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-CASAL-87.jpg]', disponivel: true },
-        { id: 20, nome: 'Família R$ 87,99', preco: 87.99, descricao: '4 Smash Original + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-FAMILIA-87.jpg]', disponivel: true },
-        { id: 21, nome: 'Família R$ 115,99', preco: 115.99, descricao: '4 JottaV Classic + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-FAMILIA-115.jpg]', disponivel: true },
-        { id: 22, nome: 'Família R$ 111,99', preco: 111.99, descricao: '4 Smash Duplo + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-FAMILIA-111.jpg]', disponivel: true },
-        { id: 23, nome: 'Família R$ 154,90', preco: 154.90, descricao: '4 Duplo Brutão + batata frita + Guaraná 1L.', imagem: '[URL-DA-IMAGEM-COMBO-FAMILIA-154.jpg]', disponivel: true }
+        { id: 12, nome: 'Combo Econômico', preco: 31.99, descricao: '1 Smash Original + batata frita + refrigerante lata.', imagem: 'imagens_combos/combo_economico.jpg', disponivel: true },
+        { id: 13, nome: 'Combo do Chef', preco: 41.99, descricao: '1 JottaV Classic + batata frita + refrigerante lata.', imagem: 'imagens_combos/combo_chef.jpg', disponivel: true },
+        { id: 14, nome: 'Combo Smash Duplo', preco: 37.99, descricao: '1 Smash Duplo + batata frita + refrigerante lata.', imagem: 'imagens_combos/combo_smash_duplo.jpg', disponivel: true },
+        { id: 15, nome: 'Combo Premium', preco: 53.99, descricao: '1 Duplo Brutão + batata frita + refrigerante lata.', imagem: 'imagens_combos/combo_premium.jpg', disponivel: true },
+        { id: 16, nome: 'Casal R$ 49,99', preco: 49.99, descricao: '2 Smash Original + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_casal_49.jpg', disponivel: true },
+        { id: 17, nome: 'Casal R$ 63,99', preco: 63.99, descricao: '2 JottaV Classic + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_casal_63.jpg', disponivel: true },
+        { id: 18, nome: 'Casal R$ 61,99', preco: 61.99, descricao: '2 Smash Duplo + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_casal_61.jpg', disponivel: true },
+        { id: 19, nome: 'Casal R$ 87,99', preco: 87.99, descricao: '2 Duplo Brutão + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_casal_87.jpg', disponivel: true },
+        { id: 20, nome: 'Família R$ 87,99', preco: 87.99, descricao: '4 Smash Original + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_familia_87.jpg', disponivel: true },
+        { id: 21, nome: 'Família R$ 115,99', preco: 115.99, descricao: '4 JottaV Classic + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_familia_115.jpg', disponivel: true },
+        { id: 22, nome: 'Família R$ 111,99', preco: 111.99, descricao: '4 Smash Duplo + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_familia_111.jpg', disponivel: true },
+        { id: 23, nome: 'Família R$ 154,90', preco: 154.90, descricao: '4 Duplo Brutão + batata frita + Guaraná 1L.', imagem: 'imagens_combos/combo_familia_154.jpg', disponivel: true }
     ],
     adicionais: [
-        { id: 24, nome: 'Creme de Requeijão', preco: 4.00, descricao: '', imagem: '[URL-DA-IMAGEM-CREME-REQUEIJAO.jpg]', disponivel: true },
-        { id: 25, nome: 'Queijo Muçarela', preco: 4.00, descricao: '', imagem: '[URL-DA-IMAGEM-QUEIJO.jpg]', disponivel: true },
-        { id: 26, nome: 'Bacon', preco: 4.00, descricao: '', imagem: '[URL-DA-IMAGEM-BACON.jpg]', disponivel: true },
-        { id: 27, nome: 'Farofa de Bacon', preco: 3.00, descricao: '', imagem: '[URL-DA-IMAGEM-FAROFA-BACON.jpg]', disponivel: true },
-        { id: 28, nome: 'Farofa de Alho', preco: 3.00, descricao: '', imagem: '[URL-DA-IMAGEM-FAROFA-ALHO.jpg]', disponivel: true },
-        { id: 29, nome: 'Molho Barbecue', preco: 2.00, descricao: '', imagem: '[URL-DA-IMAGEM-MOLHO-BARBECUE.jpg]', disponivel: true },
-        { id: 30, nome: 'Molho da Casa', preco: 2.00, descricao: '', imagem: '[URL-DA-IMAGEM-MOLHO-CASA.jpg]', disponivel: true },
-        { id: 31, nome: 'Calabresa', preco: 5.00, descricao: '', imagem: '[URL-DA-IMAGEM-ADICIONAL-CALABRESA.jpg]', disponivel: true },
-        { id: 32, nome: 'Salame', preco: 7.00, descricao: '', imagem: '[URL-DA-IMAGEM-ADICIONAL-SALAME.jpg]', disponivel: true },
-        { id: 33, nome: 'Requeijão', preco: 4.00, descricao: '', imagem: '[URL-DA-IMAGEM-ADICIONAL-REQUEIJAO.jpg]', disponivel: true }
+        { id: 24, nome: 'Creme de Requeijão', preco: 4.00, descricao: '', imagem: 'imagens_adicionais/creme_requeijao.jpg', disponivel: true },
+        { id: 25, nome: 'Queijo Muçarela', preco: 4.00, descricao: '', imagem: 'imagens_adicionais/queijo.jpg', disponivel: true },
+        { id: 26, nome: 'Bacon', preco: 4.00, descricao: '', imagem: 'imagens_adicionais/bacon.jpg', disponivel: true },
+        { id: 27, nome: 'Farofa de Bacon', preco: 3.00, descricao: '', imagem: 'imagens_adicionais/farofa_bacon.jpg', disponivel: true },
+        { id: 28, nome: 'Farofa de Alho', preco: 3.00, descricao: '', imagem: 'imagens_adicionais/farofa_alho.jpg', disponivel: true },
+        { id: 29, nome: 'Molho Barbecue', preco: 2.00, descricao: '', imagem: 'imagens_adicionais/molho_barbecue.jpg', disponivel: true },
+        { id: 30, nome: 'Molho da Casa', preco: 2.00, descricao: '', imagem: 'imagens_adicionais/molho_casa.jpg', disponivel: true },
+        { id: 31, nome: 'Calabresa', preco: 5.00, descricao: '', imagem: 'imagens_adicionais/calabresa.jpg', disponivel: true },
+        { id: 32, nome: 'Salame', preco: 7.00, descricao: '', imagem: 'imagens_adicionais/salame.jpg', disponivel: true },
+        { id: 33, nome: 'Requeijão', preco: 4.00, descricao: '', imagem: 'imagens_adicionais/requeijao.jpg', disponivel: true }
     ],
     bebidas: [
-        { id: 34, nome: 'Coca-Cola Lata', preco: 7.00, descricao: '', imagem: '[URL-DA-IMAGEM-COCA-LATA.jpg]', disponivel: true },
-        { id: 35, nome: 'Guaraná Lata', preco: 6.00, descricao: '', imagem: '[URL-DA-IMAGEM-GUARANA-LATA.jpg]', disponivel: true },
-        { id: 36, nome: 'Guaraná 1L', preco: 9.00, descricao: '', imagem: '[URL-DA-IMAGEM-GUARANA-1L.jpg]', disponivel: true }
+        { id: 34, nome: 'Coca-Cola Lata', preco: 7.00, descricao: '', imagem: 'imagens_bebidas/coca_lata.jpg', disponivel: true },
+        { id: 35, nome: 'Guaraná Lata', preco: 6.00, descricao: '', imagem: 'imagens_bebidas/guarana_lata.jpg', disponivel: true },
+        { id: 36, nome: 'Guaraná 1L', preco: 9.00, descricao: '', imagem: 'imagens_bebidas/guarana_1l.jpg', disponivel: true }
     ]
 };
 
